9c792c19f84e68a6f1401785cb22cd03e05e07b0
Jeonghoon Lee <hoon22.lee@gmail.com>
2022-04-29 14:24:38 -0400

    added .mvn folder

--- CarDealership/src/main/java/cardealership/dao/SalesDaoDB.java
+++ CarDealership/src/main/java/cardealership/dao/SalesDaoDB.java
@@ -4,10 +4,53 @@
  */
 package cardealership.dao;
 
+import cardealership.dto.Sales;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  *
  * @author Jeonghoon
  */
-public class SalesDaoDB {
+public class SalesDaoDB implements SalesDao {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @Override
+    public Sales createSales(Sales sales) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Sales> getAllSales() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Sales getSalesById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Sales deleteSalesById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean updateSales(Sales sales) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Sales> getAllSalesByUser(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Sales getSalesByVin(String vin) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
