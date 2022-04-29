8da9425755ed7e0992e126fdb7e557d7cea98918
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-29 10:26:28 -0400

    Added project skeleton

--- CarDealership/src/main/java/cardealership/CarDealershipApplication.java
+++ CarDealership/src/main/java/cardealership/CarDealershipApplication.java
@@ -1,4 +1,4 @@
-package cardealership.CarDealership;
+package cardealership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
