0fc48cc8d1e8f27657f8d51f8f6cd7ef1f74aaf3
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-04-29 10:19:27 -0400

    Add files via upload

---
+++ CarDealership/src/test/java/cardealership/CarDealership/CarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package cardealership.CarDealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
