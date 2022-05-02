fc41f317b8f24655f849e9d8f987dbe9a3639675
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-05-02 11:54:11 -0400

    Added documentation for Vehicle interface

--- CarDealership/src/main/java/cardealership/dao/VehicleDao.java
+++ CarDealership/src/main/java/cardealership/dao/VehicleDao.java
@@ -3,26 +3,62 @@ package cardealership.dao;
 import cardealership.dto.Vehicle;
 import java.util.List;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
 /**
  *
  * @author Grant
  */
 public interface VehicleDao {
+    /**
+     * Takes a vehicle object and stores it into the cardealership database.
+     * 
+     * @param vehicle, the vehicle to be input into the database.
+     * @return 
+     */
     Vehicle create(Vehicle vehicle);
-
+    
+    
+    /**
+     * Gets a vehicle and its associated data from the cardealership database.
+     * 
+     * @param vin the Vehicle Identification Number for the vehicle that the 
+     *     user wants to obtain.
+     * @return a Vehicle object that is associated with the given vin number.
+     */
     Vehicle getVehicleByVIN(String vin);
 
+    
+    /**
+     * Obtains a List of all Vehicles in the car dealership database.
+     * 
+     * @return a List containing all vehicles in the car dealership database.
+     */
     List<Vehicle> getAllVehicles();
 
+    
+    /**
+     * Deletes a vehicle associated with the given vin number.
+     * 
+     * @param vin the Vehicle Identification Number for the vehicle that the 
+     *     user wants to obtain.
+     * @return the vehicle object which is deleted from the database.
+     */
     Vehicle deleteByVIN(String vin);
 
+    
+    /**
+     * Update the vehicle object in the database.
+     * 
+     * @param vehicle
+     * @return 
+     */
     boolean updateVehicle(Vehicle vehicle);
 
-    // TODO: add more logic.
+    
+    /**
+     * Obtain all vehicles that have the given makeId.
+     * 
+     * @param makeId
+     * @return 
+     */
     List<Vehicle> getAllVehiclesByMake(int makeId);
 }
