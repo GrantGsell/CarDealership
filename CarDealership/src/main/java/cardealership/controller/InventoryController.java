b52b2d7f8743947affe89c71b7116a1692558481
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-05-03 13:35:03 -0400

    Added list of new, old car cards for inventory

--- CarDealership/src/main/java/cardealership/controller/InventoryController.java
+++ CarDealership/src/main/java/cardealership/controller/InventoryController.java
@@ -1,13 +1,18 @@
 package cardealership.controller;
 
 import cardealership.dao.VehicleDao;
+import cardealership.dto.QuickSearch;
 import cardealership.dto.Vehicle;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -21,18 +26,64 @@ public class InventoryController {
     
     @GetMapping("inventory/new")
     public String getNewInventoryPage(Model model) {
-        List<Vehicle> vehicles = vehicleDao.getAllVehicles();
-        
-        model.addAttribute("vehicleList", vehicles);
         return "inventory/new";
     }
 
     @GetMapping("inventory/used")
-    public String getUsedInventoryPage(Model model) {
-        List<Vehicle> vehicles = vehicleDao.getAllVehicles();
-        
-        model.addAttribute("vehicleList", vehicles);
-        
+    public String getUsedInventoryPage(Model model) {        
         return "inventory/used";
+    }
+    
+    @PostMapping("inventory/search")
+    @ResponseBody
+    public List<Vehicle> createProduct(@RequestBody QuickSearch search) {
+
+        return vehicleDao.getAllVehicles().stream()
+                .filter(vehicle -> {
+                    if (!search.getKeyword().isBlank()) {
+                        return vehicle.getMake().getNameMake().contains(search.getKeyword())
+                                || vehicle.getModel().getNameModel().contains(search.getKeyword())
+                                || Integer.toString(vehicle.getCarYear()).equals(search.getKeyword());
+                    }
+                    return true;
+                })
+                .filter(vehicle -> {
+                    if (search.getMinYear() > 0) {
+                        if (search.getMaxYear() > 0) {
+                            return vehicle.getCarYear() >= search.getMinYear()
+                                    && vehicle.getCarYear() <= search.getMaxYear();
+                        } else {
+                            return vehicle.getCarYear() >= search.getMinYear();
+                        }
+                    } else {
+                        if (search.getMaxYear() > 0) {
+                            return vehicle.getCarYear() <= search.getMaxYear();
+                        }
+                    }
+                    // No Minimum year and No Maximum year
+                    return true;
+                })
+                .filter(vehicle -> {
+                    if (search.getMinPrice() != null) {
+                        if (search.getMaxPrice() != null) {
+                            return vehicle.getSalePrice() >= search.getMinPrice().intValue()
+                                    && vehicle.getSalePrice() <= search.getMaxPrice().intValue();
+                        } else {
+                            return vehicle.getSalePrice() >= search.getMinPrice().intValue();
+                        }
+                    } else {
+                        if (search.getMaxPrice() != null) {
+                            return vehicle.getSalePrice() <= search.getMaxPrice().intValue();
+                        }
+                    }
+                    // No Minimum Price and No Maximum Price
+                    return true;
+                })
+                .collect(Collectors.toList());
+    }
+    
+    @GetMapping("inventory/details/{vin}")
+    public String inventoryDetailPage(@PathVariable String vin, Model model){
+        return "inventory/details";
     }
 }
