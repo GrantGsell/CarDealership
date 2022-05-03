9bf298a25436a55e6481b82659e334b693885765
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-05-02 22:30:20 -0400

    Added Model object and has it return all vehicles (temporarily)

--- CarDealership/src/main/java/cardealership/controller/InventoryController.java
+++ CarDealership/src/main/java/cardealership/controller/InventoryController.java
@@ -1,21 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cardealership.controller;
 
+import cardealership.dao.VehicleDao;
+import cardealership.dto.Vehicle;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
- * @author Jeonghoon
+ * @author Grant
  */
 @Controller
 public class InventoryController {
 
+    @Autowired
+    VehicleDao vehicleDao;
+    
     @GetMapping("inventory/new")
-    public String getNewInventoryPage() {
+    public String getNewInventoryPage(Model model) {
+        //List<Integer> numbers = new ArrayList<>();
+        List<Vehicle> numbers = vehicleDao.getAllVehicles();
+        
+        model.addAttribute("numberList", numbers);
         return "inventory/new";
     }
 
